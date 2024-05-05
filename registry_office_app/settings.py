d98e505c3b8fee232b0cc910d5909691b51734c4
qwertyqwa <petrovnamahina@gmail.com>
2024-05-05 13:59:19 +0300

    Delete and change files

--- registry_office_app/settings.py
+++ registry_office_app/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'registry_office_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'registry_office',
+        'USER': 'postgres',
+        'PASSWORD': '12344321',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
